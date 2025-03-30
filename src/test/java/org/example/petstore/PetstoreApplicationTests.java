6bef0b3a5ee513f6b188316c8cdf80fb4a48840c
Giassinne <yassinekaddouri2711@gmail.com>
2025-03-30 06:28:12 +0200

    FIX SEPERATE PROFILES FOR LOCAL AND DOCKER

--- src/test/java/org/example/petstore/PetstoreApplicationTests.java
+++ src/test/java/org/example/petstore/PetstoreApplicationTests.java
@@ -1,17 +1,20 @@
 package org.example.petstore;
 
-import org.apache.catalina.core.ApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+
 @SpringBootTest
+@ActiveProfiles("test")
 class PetStoreApplicationTests {
 
     @Test
     void contextLoads(ApplicationContext context) {
-        assertNotNull(context, "Application context should not be null");
+        assertNotNull(context, "Application context should have loaded successfully");
     }
 
 }
